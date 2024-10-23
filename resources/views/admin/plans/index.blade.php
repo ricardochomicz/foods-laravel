21bdbcd697a2553163994b0a73029f14d3bc246c
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-10-23 20:27:28 -0300

    refactor: view index plans

--- resources/views/admin/plans/index.blade.php
+++ resources/views/admin/plans/index.blade.php
@@ -7,26 +7,31 @@
 @stop
 
 @section('content')
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Plano</th>
-                <th>Preço</th>
-                <th>Descrição</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($plans as $plan)
-                <tr>
-                    <td>{{ $plan->name }}</td>
-                    <td>{{ $plan->price }}</td>
-                    <td>{{ $plan->description }}</td>
-                    <td>
-                        {{-- <a href="{{ route('admin.plans.show', $plan->id) }}" class="btn btn-primary">Detalhes</a> --}}
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-borderlles">
+                <thead>
+                    <tr>
+                        <th>Plano</th>
+                        <th>Preço</th>
+                        <th>Descrição</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>{{ $plan->name }}</td>
+                            <td>{{ moneyFormat($plan->price) }}</td>
+                            <td>{{ $plan->description }}</td>
+                            <td>
+
+                                {{-- <a href="{{ route('admin.plans.show', $plan->id) }}" class="btn btn-primary">Detalhes</a> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
