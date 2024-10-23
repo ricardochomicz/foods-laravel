398449e6247e8014dd710a9d00e29cb5693997e3
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-10-23 20:28:53 -0300

    refactor: create metods

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -13,4 +13,22 @@ class PlanController extends Controller
         $plans = Plan::all();
         return view('admin.plans.index', compact('plans'));
     }
+
+    public function create()
+    {
+        return view('admin.plans.create');
+    }
+
+    public function store(Request $request) {}
+
+    public function edit($id)
+    {
+        $plan = Plan::find($id);
+        if (!$plan) {
+            return back();
+        }
+        return view('admin.plans.edit', compact('plan'));
+    }
+
+    public function update(Request $request, $id) {}
 }
