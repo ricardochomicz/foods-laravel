6b0b840bc605de9fce9c4701bd475a8024e0a8f4
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-10-23 21:01:20 -0300

    refactor: view edit plan

--- resources/views/admin/plans/edit.blade.php
+++ resources/views/admin/plans/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content_header')
     <div class="row">
         <div class="col-sm-6">
-            <h4>Editar Plano</h4>
+            <h4>Editar Plano - {{ $plan->name }}</h4>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
